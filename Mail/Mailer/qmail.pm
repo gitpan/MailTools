d79080896d6630181310048f2761450dc00ad57f
Mark Overmeer <mark@overmeer.net>
2001-12-10 18:28:08 +0000

    Import of MARKOV/MailTools-1.42 from CPAN.
    gitpan-cpan-distribution: MailTools
    gitpan-cpan-version:      1.42
    gitpan-cpan-path:         MARKOV/MailTools-1.42.tar.gz
    gitpan-cpan-author:       MARKOV
    gitpan-cpan-maturity:     released

--- Mail/Mailer/qmail.pm
+++ Mail/Mailer/qmail.pm
@@ -1,13 +1,9 @@
-package Mail::Mailer::test;
+package Mail::Mailer::qmail;
 use vars qw(@ISA);
 require Mail::Mailer::rfc822;
 @ISA = qw(Mail::Mailer::rfc822);
 
-sub can_cc { 0 }
-
 sub exec {
     my($self, $exe, $args, $to) = @_;
-    exec('sh', '-c', 'echo "to: ' . join(" ",@{$to}) . '"; cat');
+    exec(( $exe ));
 }
-
-1;
