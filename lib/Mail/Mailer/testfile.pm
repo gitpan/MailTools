358a9c68039e3ebbdfd6c4134f5ed02b560bb815
Mark Overmeer <mark@overmeer.net>
2008-04-14 09:14:48 +0000

    Import of MARKOV/MailTools-2.03 from CPAN.
    gitpan-cpan-distribution: MailTools
    gitpan-cpan-version:      2.03
    gitpan-cpan-path:         MARKOV/MailTools-2.03.tar.gz
    gitpan-cpan-author:       MARKOV
    gitpan-cpan-maturity:     released

--- lib/Mail/Mailer/testfile.pm
+++ lib/Mail/Mailer/testfile.pm
@@ -1,12 +1,12 @@
-# Copyrights 1995-2007 by Mark Overmeer <perl@overmeer.net>.
+# Copyrights 1995-2008 by Mark Overmeer <perl@overmeer.net>.
 #  For other contributors see ChangeLog.
 # See the manual pages for details on the licensing terms.
-# Pod stripped from pm file by OODoc 1.03.
+# Pod stripped from pm file by OODoc 1.04.
 use strict;
 
 package Mail::Mailer::testfile;
 use vars '$VERSION';
-$VERSION = '2.02';
+$VERSION = '2.03';
 use base 'Mail::Mailer::rfc822';
 
 use Mail::Util qw/mailaddress/;
@@ -35,7 +35,7 @@ sub close { 1 }
 
 package Mail::Mailer::testfile::pipe;
 use vars '$VERSION';
-$VERSION = '2.02';
+$VERSION = '2.03';
 
 sub TIEHANDLE
 {   my ($class, $self) = @_;
